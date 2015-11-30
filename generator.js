43797b2a259afd05353d92090982f5caea15344b
John Beech <github@mkv25.net>
2015-11-30 21:57:17 +0000

    Added yargs to provide configuration from the command line

--- generator.js
+++ generator.js
@@ -2,7 +2,26 @@ var clean = require('./lib/path/clean');
 var host = require('./lib/host');
 var generate = require('./lib/generate');
 
-clean('build/images')
+var argv = require('yargs')
+    .default('p', 12020)
+    .describe('p', 'Port number for local template server')
+    .default('t', 'templates')
+    .describe('t', 'Template directory')
+    .default('o', 'build')
+    .describe('o', 'Output directory for assets')
+    .default('i', 'instructions')
+    .describe('i', 'Instructions directory')
+    .help('h')
+    .alias('h', 'help')
+    .epilogue('HAG is available on an ISC License, created by John Beech aka Markavian, 2015')
+    .argv;
+
+global.port = argv.p;
+global.templatesPath = argv.t;
+global.outputPath = argv.o;
+global.instructionsPath = argv.i;
+
+clean(global.outputPath)
     .then(host)
     .then(generate)
     .then(reportSuccess)
