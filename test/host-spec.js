3e83303ea6770ef873f17fcc0448d77ee20b059b
John Beech <github@mkv25.net>
2015-12-07 23:00:26 +0000

    Added test for API.host /output

--- test/host-spec.js
+++ test/host-spec.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const clean = require('../lib/path/clean');
 const complete = require('./helpers/complete');
 const read = require('../lib/path/read');
+const write = require('../lib/path/write');
 const fetch = require('../lib/path/fetch');
 const testOptions = require('./helpers/testOptions');
 const hag = require('../generator.js');
@@ -61,6 +62,30 @@ describe('API.host', function() {
                 return Promise.all([
                     fetch(serverUrl + '/templates/example-text-template.hbs'),
                     read(global.templatesPath + '/example-text-template.hbs')
+                ]);
+            })
+            .then(function(files) {
+                expect(files[0].body).to.deep.equal(files[1].toString());
+            })
+            .then(complete(done))
+            .catch(done);
+    });
+
+    it('should host generated assets on `/output`', function(done) {
+        testOptions.port++;
+        var api = hag(testOptions);
+        api.init()
+            .then(function() {
+                return read(__dirname + '/fixtures/expected-text-asset.md')
+                    .then(function(body) {
+                        return write(global.outputPath + '/example-text-asset.md', body);
+                    });
+            })
+            .then(api.host)
+            .then(function(serverUrl) {
+                return Promise.all([
+                    fetch(serverUrl + '/output/example-text-asset.md'),
+                    read(global.outputPath + '/example-text-asset.md')
                 ]);
             })
             .then(function(files) {
