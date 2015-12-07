de750192f4e953f6bc357681711e50a9c518ae33
John Beech <github@mkv25.net>
2015-12-07 22:47:18 +0000

    Added tests for API.host

---
+++ test/host-spec.js
@@ -0,0 +1,72 @@
+const expect = require('chai').expect;
+const clean = require('../lib/path/clean');
+const complete = require('./helpers/complete');
+const read = require('../lib/path/read');
+const fetch = require('../lib/path/fetch');
+const testOptions = require('./helpers/testOptions');
+const hag = require('../generator.js');
+
+describe('API.host', function() {
+
+    beforeEach(function(done) {
+        clean('temp').then(done);
+    });
+
+    afterEach(function(done) {
+        clean('temp').then(done);
+    })
+
+    it('should host instructions on `/instructions`', function(done) {
+        testOptions.port++;
+        var api = hag(testOptions);
+        api.init()
+            .then(api.host)
+            .then(function(serverUrl) {
+                return Promise.all([
+                    fetch(serverUrl + '/instructions/example-instructions.json'),
+                    read(global.instructionsPath + '/example-instructions.json')
+                ]);
+            })
+            .then(function(files) {
+                expect(JSON.parse(files[0].body)).to.deep.equal(JSON.parse(files[1].toString()));
+            })
+            .then(complete(done))
+            .catch(done);
+    });
+
+    it('should host data on `/data`', function(done) {
+        testOptions.port++;
+        var api = hag(testOptions);
+        api.init()
+            .then(api.host)
+            .then(function(serverUrl) {
+                return Promise.all([
+                    fetch(serverUrl + '/data/example-data.json'),
+                    read(global.dataPath + '/example-data.json')
+                ]);
+            })
+            .then(function(files) {
+                expect(JSON.parse(files[0].body)).to.deep.equal(JSON.parse(files[1].toString()));
+            })
+            .then(complete(done))
+            .catch(done);
+    });
+
+    it('should host templates on `/templates`', function(done) {
+        testOptions.port++;
+        var api = hag(testOptions);
+        api.init()
+            .then(api.host)
+            .then(function(serverUrl) {
+                return Promise.all([
+                    fetch(serverUrl + '/templates/example-text-template.hbs'),
+                    read(global.templatesPath + '/example-text-template.hbs')
+                ]);
+            })
+            .then(function(files) {
+                expect(files[0].body).to.deep.equal(files[1].toString());
+            })
+            .then(complete(done))
+            .catch(done);
+    });
+});
